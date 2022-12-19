f858710d1f4bc2370da5322f2680cfabe8af5efb
micarsan <miguelangel.s@260606@cesurformacion.com>
2022-12-19 11:53:37 +0100

    Terminando

--- static/app.js
+++ static/app.js
@@ -3,3 +3,13 @@
     2DAW - Cesur Málaga este
 */
 
+window.addEventListener("load", function () {
+
+    document.querySelector('button.new_client').addEventListener('click', function(){
+        document.querySelector('#new_client').classList.remove('display_none');
+    });
+    document.querySelector('button.close').addEventListener('click', function(){
+        document.querySelector('#new_client').classList.add('display_none');
+    });
+
+});
